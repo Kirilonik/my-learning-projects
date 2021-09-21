39f1121db203e2d1b86bf2a4cb47d66df67b4010
Kirilonik <kga.email.work@gmail.com>
2021-09-21 23:26:14 +0300

    [create]: create repo

---
+++ Threads_and_cicles/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace Threads_and_cicles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(168, 505);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(191, 36);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(469, 505);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(191, 36);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(783, 505);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(191, 36);
+            this.panel3.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Тыкни и картинка изменится";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "item1",
+            "item2",
+            "item3"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выбирай чееел";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.comboBox1);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(643, 42);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(292, 386);
+            this.panel4.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 129);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(267, 254);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 67);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 67);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(3, 3);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(194, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button1);
+            this.panel5.Controls.Add(this.textBox1);
+            this.panel5.Controls.Add(this.trackBar1);
+            this.panel5.Location = new System.Drawing.Point(941, 42);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 211);
+            this.panel5.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::Threads_and_cicles.Properties.Resources.Новый_проект;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 577);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Anime";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ Threads_and_cicles/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Threads_and_cicles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                Cbitem item = new Cbitem(i*3, i*7);
+                comboBox1.Items.Add(item);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool move = false;
+        Point delta;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            delta = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move)
+            {
+                pictureBox1.Location = new Point(e.X + pictureBox1.Location.X - delta.X, e.Y + pictureBox1.Location.Y - delta.Y);
+                Size size = Form1.ActiveForm.Size;
+                if(pictureBox1.Location.X < 0)
+                {
+                    pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+                }
+                if (pictureBox1.Location.Y < 0)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, 0);
+                }
+                if (pictureBox1.Location.X + pictureBox1.Size.Width > size.Width)
+                {
+                    pictureBox1.Location = new Point(size.Width - pictureBox1.Size.Width, pictureBox1.Location.Y);
+                }
+                if (pictureBox1.Location.Y + pictureBox1.Size.Height > size.Height)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, size.Height - pictureBox1.Size.Height);
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move)
+            {
+                pictureBox1.Location = new Point(e.X + pictureBox1.Location.X - delta.X, e.Y + pictureBox1.Location.Y - delta.Y);
+                Size size = Form1.ActiveForm.Size;
+                if (pictureBox1.Location.X < 0)
+                {
+                    pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+                }
+                if (pictureBox1.Location.Y < 0)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, 0);
+                }
+                if (pictureBox1.Location.X + pictureBox1.Size.Width > size.Width)
+                {
+                    pictureBox1.Location = new Point(size.Width - pictureBox1.Size.Width, pictureBox1.Location.Y);
+                }
+                if (pictureBox1.Location.Y + pictureBox1.Size.Height > size.Height)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, size.Height - pictureBox1.Size.Height);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+        int add = -2;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(pictureBox1.Width <= 10 || pictureBox1.Width >= 600)
+                add = -add;
+
+            if (pictureBox1.Width <= 10)
+                pictureBox1.Width = 12;
+            if (pictureBox1.Width >= 600)
+                pictureBox1.Width = 598;
+
+            pictureBox1.Width = pictureBox1.Width + add ;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected = comboBox1.Items[comboBox1.SelectedIndex].ToString(); // получить выбранные в выпадающем меню строку
+            string s = comboBox1.SelectedItem.ToString();
+
+            // проверка является ли выбранный элемент принадлежащим Cbitem
+            Cbitem current;
+            if(comboBox1.SelectedItem is Cbitem)
+                current = comboBox1.SelectedItem as Cbitem;
+            else
+                current = new Cbitem(0, 0);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value.ToString();
+            //textBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+        // open
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) // тут же и открываем окно для выбора файла
+            {
+                string data;
+                string panth = openFileDialog1.FileName;
+                using (StreamReader fin = new StreamReader(panth)) 
+                {
+                    data = fin.ReadToEnd();
+                    textBox2.Text = data;
+                }
+            }
+        }
+
+
+        // save
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using(StreamWriter fout = new StreamWriter(saveFileDialog1.FileName)) 
+                {
+                    fout.Write(textBox2.Text);
+                }
+                string json = "";
+                using (StreamWriter fout = new StreamWriter(saveFileDialog1.FileName + ".json"))
+                {
+                    json = Newtonsoft.Json.JsonConvert.SerializeObject(comboBox1.Items, Newtonsoft.Json.Formatting.Indented);
+                    fout.Write(json);
+                    List<object> jsonread = Newtonsoft.Json.JsonConvert.DeserializeObject<List<object>>(json);
+                    // добавил еще, что потом джон файл выводится на текстбокс1
+                    string temp = "";
+                    foreach (object item in jsonread)
+                    {
+                        temp += item.ToString();
+                        temp += " ";
+                    }
+                    textBox2.Text = temp;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        // и еще сделать чтобы примагничивалось к панельке при наведении курсора достаточно близко к нему
+        // сделать 2 прораммы которые обмениваются друг с другом даннми tcp вебсокеты какие то
+      
+    }
+
+    public class Cbitem
+    {
+        public int f1;
+        public int f2;
+
+        public Cbitem(int f1, int f2)
+        {
+            this.f1 = f1;
+            this.f2 = f2;
+        }
+
+        public override string ToString()
+        {
+            return f1.ToString() + " " + f2.ToString();
+        }
+    }
+}
